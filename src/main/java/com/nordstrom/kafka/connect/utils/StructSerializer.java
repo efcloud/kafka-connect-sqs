e8decb1251be45fca347fdac185c1fbc75fa4b5c
JavierMonton <javier.monton@ef.com>
2025-03-19 16:02:42 +0100

    Improve exceptions when parsing dates.

--- src/main/java/com/nordstrom/kafka/connect/utils/StructSerializer.java
+++ src/main/java/com/nordstrom/kafka/connect/utils/StructSerializer.java
@@ -25,13 +25,34 @@ public class StructSerializer extends JsonSerializer<Struct> {
         struct.schema().fields().forEach(field -> {
             try {
                 Object value = struct.get(field);
+
                 // Transform timestamp to defined format
                 if (timestampPattern != null && field.schema().name() != null && field.schema().name().equals(Timestamp.LOGICAL_NAME)) {
-                    value = dateFormat.format(value);
+                    if (value instanceof java.util.Date) {
+                        value = dateFormat.format((java.util.Date) value);
+                    } else {
+                        throw new IllegalArgumentException("Unsupported timestamp type: " + value.getClass());
+                    }
                 }
-                gen.writeObjectField(field.name(), value);
+
+                // Recursively serialize nested objects
+                if (value != null) {
+                    if (value instanceof Struct) {
+                        gen.writeFieldName(field.name());
+                        serialize((Struct) value, gen, serializers);
+                    } else {
+                        gen.writeObjectField(field.name(), value);
+                    }
+                } else {
+                    gen.writeObjectField(field.name(), null);
+                }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                throw new ParseException("Failed to write field " + field.name() +
+                        ", in Schema type: " + field.schema().name(), e);
+            } catch (IllegalArgumentException e) {
+                throw new ParseException("Failed to parse Timestamp field " + field.name() +
+                        ", in Schema type: " + field.schema().name() +
+                        " with value " + struct.get(field).toString(), e);
             }
         });
         gen.writeEndObject();
