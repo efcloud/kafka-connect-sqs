ccad32d6f0355b11d3d8ee17880c06ee07cff722
JavierMonton <javier.monton@ef.com>
2025-03-19 16:02:42 +0100

    Fix issues on optional Protobuf timestamps

--- src/main/test/com/nordstrom/kafka/connect/sqs/SqsSinkConnectorTaskTest.java
+++ src/main/test/com/nordstrom/kafka/connect/sqs/SqsSinkConnectorTaskTest.java
@@ -105,6 +105,51 @@ public class SqsSinkConnectorTaskTest {
     }
 
     @Test
+    public void testPutWithNestedStructAndArraysValue() throws Exception {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = dateFormat.parse("2025-01-01 01:20");
+
+        Schema nestedSchema = SchemaBuilder.struct()
+                .field("nestedField1", Schema.STRING_SCHEMA)
+                .field("nestedField2", Schema.INT32_SCHEMA)
+                .field("timestampField", Timestamp.SCHEMA)
+                .build();
+
+        Struct nestedStruct1 = new Struct(nestedSchema)
+                .put("nestedField1", "nestedTest1")
+                .put("nestedField2", 456)
+                .put("timestampField", date);
+
+        Struct nestedStruct2 = new Struct(nestedSchema)
+                .put("nestedField1", "nestedTest2")
+                .put("nestedField2", 789)
+                .put("timestampField", date);
+
+        Schema mainSchema = SchemaBuilder.struct()
+                .field("field1", Schema.STRING_SCHEMA)
+                .field("field2", Schema.INT32_SCHEMA)
+                .field("nestedArray", SchemaBuilder.array(nestedSchema).build())
+                .build();
+
+        Struct mainStruct = new Struct(mainSchema)
+                .put("field1", "test")
+                .put("field2", 123)
+                .put("nestedArray", Arrays.asList(nestedStruct1, nestedStruct2));
+
+
+        SinkRecord record = new SinkRecord("topic", 0, Schema.STRING_SCHEMA, "key", mainSchema, mainStruct, 0);
+        Collection<SinkRecord> records = Collections.singletonList(record);
+
+        task.put(records);
+
+        ArgumentCaptor<String> bodyCaptor = ArgumentCaptor.forClass(String.class);
+        verify(mockClient).send(anyString(), bodyCaptor.capture(), anyString(), anyString(), any());
+        String body = bodyCaptor.getValue();
+        System.out.println(body);
+        assertEquals("{\"field1\":\"test\",\"field2\":123,\"nestedArray\":[{\"nestedField1\":\"nestedTest1\",\"nestedField2\":456,\"timestampField\":\"2025-01-01T01:20:00Z\"},{\"nestedField1\":\"nestedTest2\",\"nestedField2\":789,\"timestampField\":\"2025-01-01T01:20:00Z\"}]}", body);
+    }
+
+    @Test
     public void testPutWithTimestampyyyyMMdd() throws Exception {
         task.objectMapper = ObjectMapperProvider.getObjectMapper("yyyy-MM-dd HH:mm:ss");
         Schema schema = SchemaBuilder.struct()
